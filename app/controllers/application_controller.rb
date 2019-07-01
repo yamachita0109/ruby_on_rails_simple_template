03d3b9f0f6992cb29bfb06f27bc6cd4d8a23319b
Yamashita Kazumasa <yamachita0109@gmail.com>
2019-07-02 03:44:17 +0900

    ログイン画面側

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :init, {only: [:index, :show, :edit, :update, :detail]}
+  def init
+    # Login check
+    puts "hoge"
+    @current_user = nil
+  end
+
 end
